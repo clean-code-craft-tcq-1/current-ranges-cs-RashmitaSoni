cd6dd5e4d9360a7c58e7606a22686e99e56f1501
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-04-30 11:58:42 +0530

    Update BatteryCurrentMeasurementsTests.cs

--- BatteryCurrentMeasurementsTests/BatteryCurrentMeasurementsTests.cs
+++ BatteryCurrentMeasurementsTests/BatteryCurrentMeasurementsTests.cs
@@ -39,10 +39,10 @@ namespace BatteryCurrentMeasurementsTests
         [TestMethod]
         public void GivenCurrentReadingsSampleList_WhenSampleListIsInput_ThenReturnNotExpectedOutput()
         {
-            List<int> currentsamplereadingslist = new List<int>() { 3, 3, 5, 4, 10, 11, 12 };
+             List<int> currentsamplereadingslist = new List<int>() { 3, 5, 4, 10, 11, 12 };
             Dictionary<string, int> expectedoutput = new Dictionary<string, int>();
-            expectedoutput.Add("3-5", 4);
-            expectedoutput.Add("10-12", 7);
+            expectedoutput.Add("3-5", 3);
+            expectedoutput.Add("10-12", 3);
             Dictionary<string, int> originaloutput = _currentreadings.GetContinuousCurrentRangeWithReadings(currentsamplereadingslist);
             Assert.AreEqual(originaloutput["3-5"], expectedoutput["3-5"]);
             Assert.AreEqual(originaloutput["10-12"], expectedoutput["10-12"]);
