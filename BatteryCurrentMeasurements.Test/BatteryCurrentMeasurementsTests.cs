c27b217bbe87b6231b81d21d00ce17194779cae1
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-05-18 13:13:17 +0530

    Update BatteryCurrentMeasurementsTests.cs

--- BatteryCurrentMeasurements.Test/BatteryCurrentMeasurementsTests.cs
+++ BatteryCurrentMeasurements.Test/BatteryCurrentMeasurementsTests.cs
@@ -1,5 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System;
+using System.Linq;
+using BatteryCurrentMeasurement;
 
 namespace BatteryCurrentMeasurementsTests
 {
@@ -11,14 +14,14 @@ namespace BatteryCurrentMeasurementsTests
         public void GivenEmptySampleList_WhenSampleListIsEmpty_ThenReturnTrue()
         {
             List<int> emptycurrentsamplelist = new List<int>();
-            bool islistempty= _currentreadings.IsCurrentSampleListEmpty(emptycurrentsamplelist);
+            bool islistempty = _currentreadings.IsCurrentReadingsListEmpty(emptycurrentsamplelist);
             Assert.IsTrue(islistempty);
         }
         [TestMethod]
         public void GivenNonEmptySampleList_WhenSampleListIsNotEmpty_ThenReturnFalse()
         {
             List<int> emptycurrentsamplelist = new List<int>() { 3, 3, 5, 4, 10, 11, 12 };
-            bool islistempty = _currentreadings.IsCurrentSampleListEmpty(emptycurrentsamplelist);
+            bool islistempty = _currentreadings.IsCurrentReadingsListEmpty(emptycurrentsamplelist);
             Assert.IsFalse(islistempty);
         }
         [TestMethod]
@@ -33,14 +36,14 @@ namespace BatteryCurrentMeasurementsTests
             Assert.AreEqual(originaloutput["10-12"], expectedoutput["10-12"]);
         }
         [TestMethod]
-        public void GivenCurrentReadingsSampleList_WhenSampleListIsInput_ThenReturnNotExpectedOutput()
+        public void GivenCurrentReadingsSampleList_WhenSampleListHasNegativeInput_ThenReturnNotExpectedOutput()
         {
-            List<int> currentsamplereadingslist = new List<int>() { 3, 5, 4, 10, 11, 12 };
+            List<int> currentsamplereadingslist = new List<int>() { -3, 3, 5, 4, 10, 11, 12 };
             Dictionary<string, int> expected = new Dictionary<string, int>();
-            expected.Add("3-5", 3);
+            expected.Add("-3-5", 4);
             expected.Add("10-12", 3);
             Dictionary<string, int> original = _currentreadings.GetContinuousCurrentRangeWithReadings(currentsamplereadingslist);
-            Assert.AreEqual(original["3-5"], expected["3-5"]);
+            Assert.AreEqual(original["-3-5"], expected["-3-5"]);
             Assert.AreEqual(original["10-12"], expected["10-12"]);
         }
     }
